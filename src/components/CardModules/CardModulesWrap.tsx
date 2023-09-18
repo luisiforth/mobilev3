db7fd9c99d47673e784c35bfb99927fbc4f79292
luisiforth <luis@iforth.com.br>
2023-09-18 17:51:31 -0300

    .

--- src/components/CardModules/CardModulesWrap.tsx
+++ src/components/CardModules/CardModulesWrap.tsx
@@ -7,3 +7,17 @@ type CardModulesWrapperProps = Pick<CardModulesProps, 'children' | 'data'>;
 export function CardModulesWrap({ children }: CardModulesWrapperProps) {
   if (data) return <S.Root.Wrap>{children}</S.Root.Wrap>;
 }
+
+{
+  /* <S.Root.Wrap>
+{data.map((item, index) => (
+  <div key={index}>
+    {React.cloneElement(children, {
+      name: item.name,
+      icon: item.icon,
+      href: item.href,
+    })}
+  </div>
+))}
+</S.Root.Wrap> */
+}
